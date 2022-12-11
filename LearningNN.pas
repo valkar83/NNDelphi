26dfec723c2b15fadf446a4be9f11bb4794351a1
Valentin Karabachian <valkar@hotmail.fr>
2022-12-11 20:58:48 +0100

    La fonction sigmoïdale est prête.

--- LearningNN.pas
+++ LearningNN.pas
@@ -4,10 +4,11 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, NeuralNetwork;
 
 type
   TForm1 = class(TForm)
+    procedure FormOnCreate(Sender: TObject);
   private
 
   public
@@ -18,7 +19,20 @@ var
   Form1: TForm1;
 
 implementation
-
+Uses
+  System.Generics.Collections;
 {$R *.dfm}
+
+procedure TForm1.FormOnCreate(Sender: TObject);
+VAR
+  LNeuralNetwork : TNeuralNetwork;
+  LList          : TList<Integer>;
+begin
+  LList := TList<Integer>.Create;
+  LList.AddRange([1,2,3]);
+  LNeuralNetwork := TNeuralNetwork.Init(LList);
+  LList.free;
+  LNeuralNetwork.Free;
+end;
 
 end.
--- NeuralNetwork.pas
+++ NeuralNetwork.pas
@@ -2,24 +2,30 @@ unit NeuralNetwork;
 
 interface
 uses
+  Math,
   Matrix,
   System.Types,
   System.Generics.Collections;
 type
   TNeuralNetwork = CLASS (TObject)
-    FListeNeuroneParCouche : TList<Integer>;
     FNbCouches             : Integer;
     FBiais                 : Array of TDoubleMatrix;
     FCoefficients          : Array of TDoubleMatrix;
 
-    constructor Init(AListeNeuroneParCouche : TList<Integer>);
+    constructor Init(CONST AListeNeuroneParCouche : TList<Integer>);
+    FUNCTION Sigmoid(Z : TDoubleMatrix) : TDoubleMatrix;
+  private
+    FListeNeuroneParCouche : TList<Integer>;
+    procedure prendreExponentiel(var Value: double);
+    procedure prendreInverse(var Value: double);
   end;
 implementation
 USes
+  MtxUtilFunc,
   RandomEng;
 { TNeuralNetwork }
 
-constructor TNeuralNetwork.Init(AListeNeuroneParCouche: TList<Integer>);
+constructor TNeuralNetwork.Init(CONST AListeNeuroneParCouche: TList<Integer>);
 var
   LI,
   LJ  : Integer;
@@ -39,10 +45,31 @@ begin
     LJ := LI - 1;
     FBiais[LI] := TDoubleMatrix.CreateRand(FListeNeuroneParCouche[LJ],
                                            FListeNeuroneParCouche[LI], raSystem, 0);
-    Inc(LJ);
   end;
+  Sigmoid(FBiais[FListeNeuroneParCouche.Count - 1]);
 
 
+
+end;
+procedure TNeuralNetwork.prendreExponentiel(var Value: double);
+begin
+  Value := exp(Value);
+end;
+
+PROCEDURE TNeuralNetwork.prendreInverse(var Value: double);
+begin
+  Value := 1 / Value;
+end;
+function TNeuralNetwork.Sigmoid(Z: TDoubleMatrix): TDoubleMatrix;
+
+begin
+  MatrixToTxtFile('Etat_initial', Z);
+  result := z.ElementwiseFunc(prendreExponentiel);
+  MatrixToTxtFile('Exponentiel', Result);
+  result := result.add(1);
+  MatrixToTxtFile('1 plus Exponentiel', Result);
+  result := result.ElementwiseFunc(prendreInverse);
+  MatrixToTxtFile('1 div 1 plus Exponentiel', Result);
 end;
 
 end.
